da4afca1de566942fb9c94238064dd9d2830b410
4PlayCode <bspringmeyer@gmail.com>
2025-02-16 08:48:54 -0600

    Add files via upload

---
+++ Home_files/43965161_002.js
@@ -0,0 +1,42 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+// HubSpot Script Loader. Please do not block this resource. See more: http://hubs.ly/H0702_H0
+
+var _hsp = window._hsp = window._hsp || [];
+_hsp.push(['addEnabledFeatureGates', []]);
+_hsp.push(['setBusinessUnitId', 0]);
+!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://web.archive.org/web/20240907024149/https://js.hs-banner.com/v2/43965161/banner.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("cookieBanner-43965161",0,{"data-cookieconsent":"ignore","data-hs-ignore":true,"data-loader":"hs-scriptloader","data-hsjs-portal":43965161,"data-hsjs-env":"prod","data-hsjs-hublet":"na1"});
+!function(e,t){if(!document.getElementById(e)){var c=document.createElement("script");c.src="https://web.archive.org/web/20240907024149/https://js.hs-analytics.net/analytics/1725676800000/43965161.js",c.type="text/javascript",c.id=e;var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(c,n)}}("hs-analytics");
+!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://web.archive.org/web/20240907024149/https://js.hscollectedforms.net/collectedforms.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("CollectedForms-43965161",0,{"crossorigin":"anonymous","data-leadin-portal-id":43965161,"data-leadin-env":"prod","data-loader":"hs-scriptloader","data-hsjs-portal":43965161,"data-hsjs-env":"prod","data-hsjs-hublet":"na1"});
+
+}
+/*
+     FILE ARCHIVED ON 02:41:49 Sep 07, 2024 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 03:24:48 Feb 16, 2025.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.612
+  exclusion.robots: 0.018
+  exclusion.robots.policy: 0.008
+  esindex: 0.01
+  cdx.remote: 22.83
+  LoadShardBlock: 87.097 (3)
+  PetaboxLoader3.datanode: 111.333 (4)
+  load_resource: 276.734
+  PetaboxLoader3.resolve: 231.249
+*/
